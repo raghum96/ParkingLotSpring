ea027433e346535bb989478b1d6bddf251f0e9e5
Raghu Mahajan <raghu7mahajan@hotmail.com>
2022-12-26 00:26:22 +0530

    Initialize Parking
    Create sample Generic parking and then created min heap as per id to map to nearest parking

---
+++ src/main/java/com/raghu/ParkingLotSpring/Initializer.java
@@ -0,0 +1,47 @@
+package com.raghu.ParkingLotSpring;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
+import com.raghu.ParkingLotSpring.Modal.ParkingArea.SingleLevelParkingStructure;
+import com.raghu.ParkingLotSpring.Modal.ParkingArea.IParkingStructure;
+import com.raghu.ParkingLotSpring.Modal.ParkingArea.ParkingBox;
+
+public class Initializer {
+	
+	public static IParkingStructure GenericParking;
+	public static PriorityQueue<ParkingBox> minBoxHeap;
+	
+	public static void init() {
+
+		// add floors/slots to parking
+		ParkingBox s1 = new ParkingBox("p1a1");
+		ParkingBox s2 = new ParkingBox("p1a2");
+		ParkingBox s3 = new ParkingBox("p1a3");
+
+		List<ParkingBox> slotList = Arrays.asList(s1, s2, s3);
+
+		// add parkings
+
+		GenericParking = new SingleLevelParkingStructure("p1", "My Parking", "Mumbai", slotList);
+		// myp.setSlots(slotList);
+
+		// parking ready
+		
+		minBoxHeap= new PriorityQueue<>(new Comparator<ParkingBox>() {
+
+			@Override
+			public int compare(ParkingBox o1, ParkingBox o2) {
+				// TODO Auto-generated method stub
+				return o1.getId().compareTo(o2.getId());
+			} 
+		});
+		
+		minBoxHeap.addAll(GenericParking.getParkingBoxList());
+
+
+	}
+		
+}
