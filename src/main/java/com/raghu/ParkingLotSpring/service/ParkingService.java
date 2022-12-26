6cbf2d008449807ad490bc3c4fed8fddbcfd7e0b
Raghu Mahajan <raghu7mahajan@hotmail.com>
2022-12-26 20:02:43 +0530

    Parking service
    To get nearest slot form min heap

---
+++ src/main/java/com/raghu/ParkingLotSpring/service/ParkingService.java
@@ -0,0 +1,14 @@
+package com.raghu.ParkingLotSpring.service;
+
+import com.raghu.ParkingLotSpring.Initializer;
+import com.raghu.ParkingLotSpring.Modal.ParkingArea.ParkingBox;
+
+public class ParkingService {
+	public ParkingBox getNearestSlot() {
+		ParkingBox slot; // get slot here
+		// slot = Initializer.GenericParking.getParkingBoxList().get(1);
+		slot = Initializer.minBoxHeap.poll();
+		System.out.println("Got slot as : " + slot.getId());
+		return slot;
+	}
+}
