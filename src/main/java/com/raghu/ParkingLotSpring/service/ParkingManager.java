e55ada8d4c22002b8fde859b4778af8c393438d7
Raghu Mahajan <raghu7mahajan@hotmail.com>
2022-12-27 22:04:55 +0530

    Parking Manager
    Added counter for Parking ID
    Added unpark by Parking iD

--- src/main/java/com/raghu/ParkingLotSpring/service/ParkingManager.java
+++ src/main/java/com/raghu/ParkingLotSpring/service/ParkingManager.java
@@ -2,35 +2,41 @@ package com.raghu.ParkingLotSpring.service;
 
 import java.util.Date;
 
-import com.raghu.ParkingLotSpring.Initializer;
 import com.raghu.ParkingLotSpring.Modal.ParkingArea.ParkingBox;
 import com.raghu.ParkingLotSpring.Modal.Vehicles.Vehicle;
 import com.raghu.ParkingLotSpring.Modal.facility.ParkingSlot;
 import static com.raghu.ParkingLotSpring.SavedData.*;
 
 public class ParkingManager {
+	private static int PARKING_ID_COUNTER=1;
 	
 	ParkingService parkingService = new ParkingService();
 
 	public ParkingSlot park(Vehicle vehicle) {
 		
 		//getSlot
-		
 		ParkingBox slot=parkingService.getNearestSlot();
 		slot.setStatus(true);
 		
 		//add vehicle
-		
-		
+	
 		//provide slot
 		ParkingSlot parkingSlot = new ParkingSlot();
 		parkingSlot.setSlot(slot);
-		parkingSlot.setParkingId(1);
+		parkingSlot.setParkingId(PARKING_ID_COUNTER++);
 		parkingSlot.setInTime(new Date());
 		parkingSlot.setVehicle(vehicle);
 		
+		//add to persistence
+		PARKING_TICKETS.put(parkingSlot.getParkingId(), parkingSlot);
 		
 		return parkingSlot;
+	}
+	
+	public void unParkByParkingID(int parkingId) {
+		ParkingSlot parkingSlot=PARKING_TICKETS.get(parkingId);
+		parkingSlot.setOutTime(new Date());
+		unPark(parkingSlot.getSlot().getId());		
 	}
 	
 	public void unPark(String slotId) {
