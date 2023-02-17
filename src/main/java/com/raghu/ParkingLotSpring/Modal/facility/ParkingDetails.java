c0fe1c29fa137aa1f27ea6a95e8dfc58794c680d
Raghu Mahajan <raghu7mahajan@hotmail.com>
2023-02-17 12:44:32 +0530

    Modified model

--- src/main/java/com/raghu/ParkingLotSpring/Modal/facility/ParkingDetails.java
+++ src/main/java/com/raghu/ParkingLotSpring/Modal/facility/ParkingDetails.java
@@ -1,12 +1,11 @@
 package com.raghu.ParkingLotSpring.Modal.facility;
 
-import com.raghu.ParkingLotSpring.Modal.Vehicles.Vehicle;
-
 public class ParkingDetails {
 	int id;
-	Vehicle vehicle;
 	ParkingSlot parkingslot;
 	
 	//add fare
+	
+	
 
 }
